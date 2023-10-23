ba088a8ff2b52f7b7024a5c4e5b87d65e0b45e1c
Daniel Sequeira <danielsequeira22@gmail.com>
2023-10-23 05:58:34 -0600

    Funcionalidad boton agregar

--- Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
+++ Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
@@ -1,3 +1,10 @@
-﻿Public Class FrmPrincipal
-
+﻿Imports System.IO
+Public Class FrmPrincipal
+    Dim swEscritor As StreamWriter
+    Dim swLector As StreamReader
+    Private Sub BtnAgregar_Click(sender As Object, e As EventArgs) Handles BtnAgregar.Click
+        swEscritor = New StreamWriter("D:\Archivo.txt", False)
+        swEscritor.WriteLine(TextBox1.Text)
+        swEscritor.Close()
+    End Sub
 End Class
