d2e0c91b173c524bbe36cc641b803865397a0f8f
Daniel Sequeira <danielsequeira22@gmail.com>
2023-10-24 17:28:38 -0600

    Codigo version funcional, pendiente contador

--- Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
+++ Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
@@ -1,10 +1,14 @@
 ﻿Imports System.IO
 Public Class FrmPrincipal
-    Dim swEscritor As StreamWriter
-    Dim swLector As StreamReader
+    Dim cantidad As Integer = 0
     Private Sub BtnAgregar_Click(sender As Object, e As EventArgs) Handles BtnAgregar.Click
-        swEscritor = New StreamWriter("D:\Archivo.txt", False)
-        swEscritor.WriteLine(TextBox1.Text)
-        swEscritor.Close()
+        Dim agregarInfo As String = tbxAgregarInfo.Text
+        If agregarInfo <> "" Then
+            listaDatos.Items.Add(agregarInfo)
+            cantidad += 1
+            tbxAgregarInfo.Clear()
+
+
+        End If
     End Sub
 End Class
