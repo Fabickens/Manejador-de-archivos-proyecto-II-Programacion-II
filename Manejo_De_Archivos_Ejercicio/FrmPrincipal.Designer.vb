e2e20f968bc246f1e370f2253c5beb6a798a2902
Daniel Sequeira <danielsequeira22@gmail.com>
2023-11-21 17:34:26 -0600

    estructura del form principal

--- Manejo_De_Archivos_Ejercicio/FrmPrincipal.Designer.vb
+++ Manejo_De_Archivos_Ejercicio/FrmPrincipal.Designer.vb
@@ -29,11 +29,25 @@ Partial Class FrmPrincipal
         BtnSobrescribir = New Button()
         listaDatos = New ListBox()
         lblTotalReg = New Label()
+        Panel1 = New Panel()
+        Label1 = New Label()
+        Panel2 = New Panel()
+        Label2 = New Label()
+        Label3 = New Label()
+        Panel3 = New Panel()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox1 = New TextBox()
+        Label7 = New Label()
+        Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
         SuspendLayout()
         ' 
         ' BtnCrear
         ' 
-        BtnCrear.Location = New Point(44, 302)
+        BtnCrear.Location = New Point(22, 43)
         BtnCrear.Name = "BtnCrear"
         BtnCrear.Size = New Size(82, 23)
         BtnCrear.TabIndex = 0
@@ -42,14 +56,14 @@ Partial Class FrmPrincipal
         ' 
         ' tbxAgregarInfo
         ' 
-        tbxAgregarInfo.Location = New Point(12, 40)
+        tbxAgregarInfo.Location = New Point(87, 24)
         tbxAgregarInfo.Name = "tbxAgregarInfo"
-        tbxAgregarInfo.Size = New Size(282, 23)
+        tbxAgregarInfo.Size = New Size(115, 23)
         tbxAgregarInfo.TabIndex = 1
         ' 
         ' BtnEliminar
         ' 
-        BtnEliminar.Location = New Point(172, 302)
+        BtnEliminar.Location = New Point(361, 43)
         BtnEliminar.Name = "BtnEliminar"
         BtnEliminar.Size = New Size(75, 23)
         BtnEliminar.TabIndex = 2
@@ -58,7 +72,7 @@ Partial Class FrmPrincipal
         ' 
         ' BtnAgregar
         ' 
-        BtnAgregar.Location = New Point(44, 69)
+        BtnAgregar.Location = New Point(133, 43)
         BtnAgregar.Name = "BtnAgregar"
         BtnAgregar.Size = New Size(75, 23)
         BtnAgregar.TabIndex = 3
@@ -67,7 +81,7 @@ Partial Class FrmPrincipal
         ' 
         ' BtnSobrescribir
         ' 
-        BtnSobrescribir.Location = New Point(172, 69)
+        BtnSobrescribir.Location = New Point(239, 43)
         BtnSobrescribir.Name = "BtnSobrescribir"
         BtnSobrescribir.Size = New Size(86, 23)
         BtnSobrescribir.TabIndex = 4
@@ -78,35 +92,146 @@ Partial Class FrmPrincipal
         ' 
         listaDatos.FormattingEnabled = True
         listaDatos.ItemHeight = 15
-        listaDatos.Location = New Point(12, 120)
+        listaDatos.Location = New Point(51, 23)
         listaDatos.Name = "listaDatos"
-        listaDatos.Size = New Size(282, 154)
+        listaDatos.Size = New Size(315, 109)
         listaDatos.TabIndex = 5
         ' 
         ' lblTotalReg
         ' 
         lblTotalReg.AutoSize = True
-        lblTotalReg.Location = New Point(12, 277)
+        lblTotalReg.Location = New Point(506, 50)
         lblTotalReg.Name = "lblTotalReg"
         lblTotalReg.Size = New Size(12, 15)
         lblTotalReg.TabIndex = 6
         lblTotalReg.Text = "*"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(BtnCrear)
+        Panel1.Controls.Add(BtnAgregar)
+        Panel1.Controls.Add(BtnEliminar)
+        Panel1.Controls.Add(BtnSobrescribir)
+        Panel1.Location = New Point(31, 24)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(459, 98)
+        Panel1.TabIndex = 7
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(22, -15)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(73, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Operaciones"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(Label7)
+        Panel2.Controls.Add(TextBox1)
+        Panel2.Controls.Add(Label6)
+        Panel2.Controls.Add(Label5)
+        Panel2.Controls.Add(tbxAgregarInfo)
+        Panel2.Controls.Add(lblTotalReg)
+        Panel2.Location = New Point(37, 144)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(582, 85)
+        Panel2.TabIndex = 8
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(63, 135)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(114, 17)
+        Label2.TabIndex = 0
+        Label2.Text = "Datos del Cliente"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(20, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(84, 17)
+        Label3.TabIndex = 5
+        Label3.Text = "Operaciones"
+        ' 
+        ' Panel3
+        ' 
+        Panel3.Controls.Add(Label4)
+        Panel3.Controls.Add(listaDatos)
+        Panel3.Location = New Point(39, 262)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(413, 145)
+        Panel3.TabIndex = 9
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(31, 3)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(69, 17)
+        Label4.TabIndex = 0
+        Label4.Text = "Resultado"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(16, 32)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(56, 15)
+        Label5.TabIndex = 2
+        Label5.Text = "Nombres"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(233, 30)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(56, 15)
+        Label6.TabIndex = 3
+        Label6.Text = "Apellidos"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(315, 24)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(469, 24)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(83, 15)
+        Label7.TabIndex = 7
+        Label7.Text = "Total Registros"
         ' 
         ' FrmPrincipal
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.ActiveCaption
-        ClientSize = New Size(311, 350)
-        Controls.Add(lblTotalReg)
-        Controls.Add(listaDatos)
-        Controls.Add(BtnSobrescribir)
-        Controls.Add(BtnAgregar)
-        Controls.Add(BtnEliminar)
-        Controls.Add(tbxAgregarInfo)
-        Controls.Add(BtnCrear)
+        ClientSize = New Size(654, 419)
+        Controls.Add(Panel3)
+        Controls.Add(Label2)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
         Name = "FrmPrincipal"
         Text = "Manejo de Archivos"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -118,4 +243,15 @@ Partial Class FrmPrincipal
     Friend WithEvents BtnSobrescribir As Button
     Friend WithEvents listaDatos As ListBox
     Friend WithEvents lblTotalReg As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
--- Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
+++ Manejo_De_Archivos_Ejercicio/FrmPrincipal.vb
@@ -81,5 +81,8 @@ Public Class FrmPrincipal
         End If
     End Sub
 
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
 
